23489576bb3811ff321185cb0f0c7696e15d8087
JJIIIINN <kgb21537@gmail.com>
2024-01-17 14:35:43 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,10 @@ import './App.css';
 
 function App() {
 	const [count, setCount] = useState(0);
-
+	console.log('aa');
 	return (
 		<>
 			<div>
-
-
-        
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
@@ -21,16 +18,12 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
 }
